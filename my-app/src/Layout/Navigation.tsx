1b101bf1ccf1e8863280a70bf6032594bffce0d6
Tuna523 <duya1010@naver.com>
2023-04-20 18:30:46 +0900

    useState를 추가해 봤고 버튼을 누르면 메뉴들이 등장해요

--- my-app/src/Layout/Navigation.tsx
+++ my-app/src/Layout/Navigation.tsx
@@ -1,10 +1,20 @@
-import React, {Component} from "react";
+import React, {Component, useState} from "react";
 import styled from 'styled-components';
+// let menuBool = false;
 
 function Navigation(props:any) {
+    const [menuBool, setMenuBool] = useState<any>(false);
     return (
         <Nav>
             <NavList>
+            <button style={{display: 'flex'}} onClick={(e)=>{e.preventDefault(); setMenuBool(!menuBool); console.log(menuBool);}}>
+                <img
+                width='24px'
+                height='24px'
+                src="https://www.nicepng.com/png/detail/14-147111_menu-button-png-menu-button-icon-png.png"></img>
+            </button>
+            { menuBool ?
+            <>
                 <NavItem>NavItem1</NavItem>
                 <NavItem>NavItem2</NavItem>
                 <NavItem>NavItem3</NavItem>
@@ -12,6 +22,9 @@ function Navigation(props:any) {
                 <NavItem>NavItem5</NavItem>
                 <NavItem>NavItem5</NavItem>
                 <NavItem>NavItem5</NavItem>
+                </>
+                : <></>
+            }
             </NavList>
         </Nav>
     );
@@ -23,23 +36,30 @@ const Nav = styled.div`
     width: 100%;
     height: 30px;
     border-bottom: 1px solid #888888;
-    z-index: 1;
-    
-    // display: block;
-    // &:hover{
-    //     display: none;
-    //     background-color: black;
-    // }
+
+`
+
+const OpenButton = styled.button`
+    display: flex;
 `
 
 const NavList = styled.div`
     width: 1080px;
     display: flex;
     margin: 0 auto;
+        z-index: 1;
+    if(menuBool == false){
+        display: none;
+    }
+    // &:hover{
+    //     display: none;
+    //     background-color: black;
+    // }
 `
 
 const NavItem = styled.div`
     width: 100%;
     margin-top: 5px;
     display: flex;
+    margin-left: 15px;
 `
